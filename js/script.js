c9dcc7bd96bb8ac10d93e93541e30f0a07fe1bcc
cdemi <christopher.demicoli@outlook.com>
2021-03-17 00:02:11 +0100

    Got rid of jQuery

--- js/script.js
+++ js/script.js
@@ -1,26 +1,17 @@
-
-$(function () {
-    $("#toggle").click(function(){
-        
-       if( $("#navigation").hasClass("hidden")){
-        $("#navigation").removeClass("hidden")
-       }else{
-        $("#navigation").addClass("hidden")
-       }
-
-        return false;
-    });
-
-    new TypeIt("#heroTyper", {
-        speed: 75,
-        loop: true,
-        waitUntilVisible: true,
-        cursorChar: '_'
-      })
-        .type("reliable solutions", {delay: 1500}).move(-10).delete(-8)
-        .type("ingenious", {delay: 1500}).delete(-9)
-        .type("cutting edge", {delay: 1500}).delete(-12)
-        .type("scalable", {delay: 1500}).delete(-8)
-        .type("innovative", {delay: 5000})
-        .go();
+document.getElementById("toggle").addEventListener("click", function(e){
+  document.getElementById("navigation").classList.toggle("hidden");
+  e.preventDefault();
 });
+
+new TypeIt("#heroTyper", {
+  speed: 75,
+  loop: true,
+  waitUntilVisible: true,
+  cursorChar: '_'
+})
+  .type("reliable solutions", {delay: 1500}).move(-10).delete(-8)
+  .type("ingenious", {delay: 1500}).delete(-9)
+  .type("cutting edge", {delay: 1500}).delete(-12)
+  .type("scalable", {delay: 1500}).delete(-8)
+  .type("innovative", {delay: 5000})
+  .go();
