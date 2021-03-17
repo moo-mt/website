2ec76ece194fd5c403887e55d91db94d22b79fdb
cdemi <christopher.demicoli@outlook.com>
2021-03-17 11:45:58 +0100

    Added sky-colored shadow to white text

--- js/script.js
+++ js/script.js
@@ -1,12 +1,11 @@
 new TypeIt("#heroTyper", {
   speed: 75,
   loop: true,
-  waitUntilVisible: true,
-  cursorChar: '_'
+  waitUntilVisible: true
 })
   .type("reliable solutions", {delay: 1500}).move(-10).delete(-8)
   .type("ingenious", {delay: 1500}).delete(-9)
   .type("cutting edge", {delay: 1500}).delete(-12)
   .type("scalable", {delay: 1500}).delete(-8)
-  .type("innovative", {delay: 5000})
+  .type("innovative", {delay: 10000})
   .go();
