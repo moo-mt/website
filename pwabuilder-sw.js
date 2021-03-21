0fb81c71700ec6479791c718b0b7a065fa45e3f3
cdemi <christopher.demicoli@outlook.com>
2021-03-21 22:46:52 +0100

    Improved PWA

--- pwabuilder-sw.js
+++ pwabuilder-sw.js
@@ -1,5 +1,3 @@
-const CACHE = "pwabuilder-networkfirst";
-
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
 
 self.addEventListener("message", (event) => {
@@ -9,8 +7,47 @@ self.addEventListener("message", (event) => {
 });
 
 workbox.routing.registerRoute(
-  new RegExp('/*'),
-  new workbox.strategies.NetworkFirst({
-    cacheName: CACHE
+  new RegExp(/.*\/images.*/),
+  new workbox.strategies.StaleWhileRevalidate({
+    cacheName: "localImages-StaleWhileRevalidate"
   })
 );
+
+workbox.routing.registerRoute(
+  new RegExp(/.*fontawesome.com.*/),
+  new workbox.strategies.StaleWhileRevalidate({
+    cacheName: "fontawesome-StaleWhileRevalidate"
+  })
+);
+
+workbox.routing.registerRoute(
+  new RegExp(/.*gstatic.com.*/),
+  new workbox.strategies.StaleWhileRevalidate({
+    cacheName: "gstatic.com-StaleWhileRevalidate"
+  })
+);
+
+workbox.routing.registerRoute(
+  new RegExp(/.*cdn.jsdelivr.net.*/),
+  new workbox.strategies.StaleWhileRevalidate({
+    cacheName: "cdn.jsdelivr.net-StaleWhileRevalidate"
+  })
+);
+
+workbox.routing.registerRoute(
+  new RegExp(/.*fonts.googleapis.com.*/),
+  new workbox.strategies.StaleWhileRevalidate({
+    cacheName: "fonts.googleapis.com-StaleWhileRevalidate"
+  })
+);
+
+workbox.routing.registerRoute(
+  new RegExp(/.*(css|png|js).*/),
+  new workbox.strategies.StaleWhileRevalidate({
+    cacheName: "misc-static-StaleWhileRevalidate"
+  })
+);
+
+workbox.routing.setDefaultHandler(new workbox.strategies.NetworkFirst({
+  cacheName: "default-NetworkFirst"
+}));
