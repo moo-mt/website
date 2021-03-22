db3e2d595be89065e9ba619c9e6662621770fd7b
cdemi <christopher.demicoli@outlook.com>
2021-03-22 15:49:12 +0100

    testing out glowing icons

--- js/script.js
+++ js/script.js
@@ -9,9 +9,30 @@ new TypeIt("#heroTyper", {
   waitUntilVisible: true,
   cursorChar: '_'
 })
-  .type("<strong>reliable</strong> solutions", {delay: 1500}).move(-10).delete(-8)
-  .type("<strong>ingenious</strong>", {delay: 1500}).delete(-9)
-  .type("<strong>cutting edge</strong>", {delay: 1500}).delete(-12)
-  .type("<strong>scalable</strong>", {delay: 1500}).delete(-8)
-  .type("<strong>innovative</strong>", {delay: 10000})
+  .type("<strong>reliable</strong> solutions", { delay: 1500 }).move(-10).delete(-8)
+  .type("<strong>ingenious</strong>", { delay: 1500 }).delete(-9)
+  .type("<strong>cutting edge</strong>", { delay: 1500 }).delete(-12)
+  .type("<strong>scalable</strong>", { delay: 1500 }).delete(-8)
+  .type("<strong>innovative</strong>", { delay: 10000 })
   .go();
+
+let callback = (entries, observer) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting)
+      entry.target.classList.add("highlighted");
+    else
+      entry.target.classList.remove("highlighted");
+  });
+};
+
+let observer = new IntersectionObserver(callback, {
+  threshold: 1.0
+});
+
+document.querySelectorAll('.industry').forEach(target => {
+  observer.observe(target);
+});
+
+document.querySelectorAll('.experience').forEach(target => {
+  observer.observe(target);
+});
